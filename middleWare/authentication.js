74e5bc884c9fc7e73cd56ea22f417fc0c850cfc3
Ganesh Kumar <mrganeshofficial8@gmail.com>
2023-12-07 17:30:12 +0530

    auth middleware created

---
+++ middleWare/authentication.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  let token = req.headers.authorization?.split(" ")[1];
+  if (token) {
+    jwt.verify(token, "ganesh", (err, decoded) => {
+      if (decoded) {
+        let date = new Date().toLocaleString();
+        req.body = {
+          ...req.body,
+          username: decoded.username,
+          userId: decoded.userId,
+          date: date,
+        };
+        next();
+      } else {
+        res.status(400).json({ msg: "Please Login" });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: "Please Login" });
+  }
+};
+
+module.exports(auth);
